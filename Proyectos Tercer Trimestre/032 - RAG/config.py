d340af44d10df0265bf49a11a93b02245cffc927
Javier Carrasco <javicarrascoferrus@gmail.com>
2026-05-16 09:39:05 +0200

    Create config.py

---
+++ Proyectos Tercer Trimestre/032 - RAG/config.py
@@ -0,0 +1,15 @@
+CHROMA_DIR = "chroma_db"
+COLLECTION_NAME = "manual_tecnico"
+
+OLLAMA_EMBED_URL = "http://127.0.0.1:11434/api/embed"
+OLLAMA_GENERATE_URL = "http://127.0.0.1:11434/api/generate"
+
+EMBED_MODEL = "nomic-embed-text:v1.5"
+TEXT_MODEL = "qwen2.5:3b-instruct"
+
+PDF_INPUT = "documentos/manual_mysql.pdf"
+TXT_OUTPUT = "documentos/manual_mysql.txt"
+
+CHUNK_WORDS = 300
+CHUNK_OVERLAP = 60
+TOP_K = 6
